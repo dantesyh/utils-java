4232f4c14e6b22b95a6e5946a9eb924b77d15907
dante <dantesyh123@qq.com>
2023-09-01 18:23:12 +0800

    个人工具类 增加json转数据和list对象

--- src/main/java/dantesyh/utils/JsonUtils.java
+++ src/main/java/dantesyh/utils/JsonUtils.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Array;
 import java.lang.reflect.Type;
 import java.util.List;
 
